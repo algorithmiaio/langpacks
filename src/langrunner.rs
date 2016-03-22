522462e8ebd53928f9bd4db12dbf4500bbbb8600
Anthony Nowell <anthony@algorithmia.com>
2016-03-22 16:36:35 -0700

    refactor langserver - support for async notifiers

---
+++ src/langrunner.rs
@@ -0,0 +1,93 @@
+
+use serde_json::ser;
+use serde_json::de::StreamDeserializer;
+use serde_json::Value;
+use std::env;
+use std::io::{Read, BufRead, BufReader};
+use std::fs::File;
+use std::process::{Command, ChildStdin, Stdio};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use time::PreciseTime;
+
+const ALGOOUT: &'static str = "/tmp/algoout";
+
+
+pub struct LangRunner {
+    pub child_stdin: Mutex<ChildStdin>,
+    pub child_stdout: Arc<Mutex<Vec<String>>>, //TODO: Option - we often don't care about stdout
+}
+
+impl LangRunner {
+    pub fn new() -> LangRunner {
+        let mut path = env::current_dir().expect("Failed to get working directory");
+        path.push("bin/pipe");
+        println!("Running {:?}", path);
+        let mut child = Command::new(path)
+                                .stdin(Stdio::piped())
+                                .stdout(Stdio::piped())
+                                .stderr(Stdio::null())
+                                .spawn()
+                                .unwrap_or_else(|e| { panic!("failed to execute child: {}", e) });
+
+        let stdin = child.stdin.take().expect("Failed to open runner's STDIN");
+        let stdout = child.stdout.take().expect("Failed to open runner's STDOUT");
+
+        let child_stdout = Arc::new(Mutex::new(Vec::new()));
+
+        let arc_stdout = child_stdout.clone();
+        thread::spawn(move|| {
+            let reader = BufReader::new(stdout);
+            for line in reader.lines() {
+              let mut lines = arc_stdout.lock().expect("Failed to get lock on stdout lines");
+              lines.push(line.expect("Failed to read line"));
+            }
+        });
+
+        LangRunner{
+            child_stdin: Mutex::new(stdin),
+            child_stdout: child_stdout,
+        }
+    }
+
+    pub fn wait_for_response(&self) -> Result<String, String> {
+        let start = PreciseTime::now();
+
+        // Note: Opening a FIFO read-only pipe blocks until a writer opens it. Would be nice to open with O_NONBLOCK
+        let algoout = File::open(ALGOOUT).expect("Failed to open ALGOOUT pipe");
+
+        // Collect runner output from JSON stream - reads and deserializes the single next JSON Value on algout
+        let mut algoout_stream: StreamDeserializer<Value, _> = StreamDeserializer::new(algoout.bytes());
+        let mut output = algoout_stream.next().expect("Failed to read next JSON value from stream").expect("Failed to deserialize next JSON value from stream");
+        let duration = start.to(PreciseTime::now());
+        let duration_micro = duration.num_microseconds().unwrap() as f64 / 1_000_000f64;
+
+        // Collect buffered stdout - grab lock on child_stdout, and join all the buffered lines
+        let mut algo_stdout;
+        let arc_stdout = self.child_stdout.clone();
+        {
+          let mut lines = arc_stdout.lock().expect("Failed to get lock on stdout lines");
+          algo_stdout = lines.join("\n");
+          let _ = algo_stdout.pop();
+          lines.clear();
+        }
+
+        // Augment runner output
+        match output.as_object_mut() {
+            Some(map) => match map.get_mut("metadata") {
+                Some(metadata) => {
+                    let metadata_obj = metadata.as_object_mut().unwrap();
+                    metadata_obj.insert(s!("duration"), Value::F64(duration_micro));
+                    if !algo_stdout.is_empty() {
+                        metadata_obj.insert(s!("stdout"), Value::String(algo_stdout));
+                    }
+                },
+                None => panic!("TODO: do we nee to augment error response?"),
+            },
+            None => panic!("Output not a valid structure"),
+        };
+
+        let response = ser::to_string(&output).expect("Failed to serialize respons JSON");
+        Ok(response)
+    }
+}
---
+++ src/langserver.rs
@@ -0,0 +1,132 @@
+use base64;
+use hyper::client::Client;
+use hyper::mime::{Mime, TopLevel, SubLevel};
+use hyper::header::ContentType;
+use hyper::server::{Handler, Request, Response};
+use hyper::status::StatusCode;
+use hyper::{Url, Get, Post};
+use serde_json::{ser, de};
+use serde_json::Value;
+use serde_json::builder::ObjectBuilder;
+use std::io::{Read, Write};
+use std::sync::Arc;
+use std::thread;
+
+use super::langrunner::LangRunner;
+
+pub enum LangServerMode {
+    Sync,
+    Async(Url),
+}
+
+pub struct LangServer {
+    runner: Arc<LangRunner>,
+    mode: Arc<LangServerMode>,
+}
+
+impl LangServer {
+    pub fn new(mode: LangServerMode) -> LangServer {
+        LangServer{
+            runner: Arc::new(LangRunner::new()),
+            mode: Arc::new(mode),
+        }
+    }
+
+    fn build_input(&self, mut req: Request) -> Result<Value, String> {
+        match req.headers.get() {
+            // "application/json"
+            Some(&ContentType(Mime(TopLevel::Application, SubLevel::Json, _))) => {
+                let raw: Value = de::from_reader(req).expect("Failed to deserialize JSON request");
+                Ok(ObjectBuilder::new()
+                    .insert("content_type", "json")
+                    .insert("data", raw)
+                    .unwrap())
+            },
+            // "text/plain"
+            Some(&ContentType(Mime(TopLevel::Text, SubLevel::Plain, _))) => {
+                let mut raw = String::new();
+                let _ = req.read_to_string(&mut raw).expect("Failed to read request");
+                Ok(ObjectBuilder::new()
+                    .insert("content_type", "text")
+                    .insert("data", Value::String(raw))
+                    .unwrap())
+            },
+            // "application/octet-stream"
+            Some(&ContentType(Mime(TopLevel::Application, SubLevel::Ext(_), _))) => {
+                // TODO: verify sublevel is actually "octet-stream"
+                let mut raw: Vec<u8> = vec![];
+                let _ = req.read_to_end(&mut raw).expect("Failed to read request");
+                let b64_bytes = base64::u8en(&raw).expect("Failed encode request as base64");
+                let b64_string = String::from_utf8(b64_bytes).expect("Failed to create string from base64 bytes");
+                Ok(ObjectBuilder::new()
+                    .insert("content_type", "binary")
+                    .insert("data", Value::String(b64_string))
+                    .unwrap())
+            },
+            _ => Err(jsonerr!("Missing ContentType")),
+        }
+    }
+
+
+
+    fn run_algorithm(&self, req: Request) -> Result<Option<String>, String> {
+        // TODO: freak out if another request is in progress
+
+        let input_value = self.build_input(req).expect("Failed to build algorithm input from request");
+
+        // Get a lock on the child stdin/stdout handle
+        let arc_runner = self.runner.clone();
+        let ref child_stdin = arc_runner.child_stdin;
+
+        let mut stdin = child_stdin.lock().expect("Failed to get lock on runner's STDIN");
+
+        // Start piping data -
+        ser::to_writer(&mut *stdin, &input_value).expect("Failed to write input to runner's STDIN");
+        stdin.write(b"\n").expect("Failed to write new line to runner's STDIN");
+
+        let mode = self.mode.clone();
+        match &*mode {
+            &LangServerMode::Sync => {
+              let response = arc_runner.wait_for_response().expect("Failed waiting for response");
+              Ok(Some(response))
+            },
+            &LangServerMode::Async(ref url) => {
+              let callback_url = url.clone();
+              let arc_runner = self.runner.clone();
+              thread::spawn( move|| {
+                  let response = arc_runner.wait_for_response().expect("Failed waiting for response");
+                  if let Err(err) = Client::new()
+                                        .post(callback_url)
+                                        .header(ContentType::json())
+                                        .body(&response)
+                                        .send() {
+                      println!("Failed to send notification that request completed: {}", err);
+                  }
+              });
+              Ok(None)
+            },
+        }
+    }
+}
+
+impl Handler for LangServer {
+    fn handle(&self, req: Request, mut res: Response) {
+        let route = format!("{} {}", req.method, req.uri);
+        println!("{} (start)", route);
+
+        let (status, output) = match req.method {
+            Get => (StatusCode::Ok, jsonres!("LangServer alive.")),
+            Post => match self.run_algorithm(req) {
+                Ok(Some(out)) => (StatusCode::Ok, out),
+                Ok(None) => (StatusCode::Accepted, jsonres!("Algorithm started.")),
+                Err(err) => (StatusCode::BadRequest, err),
+            },
+            _ => (StatusCode::MethodNotAllowed, jsonerr!("Method not allowed")),
+        };
+
+        println!("{} (complete)", route);
+        res.headers_mut().set(ContentType::json());
+        *res.status_mut() = status;
+        res.send(output.as_bytes()).unwrap();
+    }
+}
