d445a8d76916ae593bfe1b9cbe24ba06af39c154
James Athappilly <jamesatha@gmail.com>
2016-03-31 16:37:14 -0700

    Making the apply method take a callback

--- javascript/examples/hello/bin/pipe.js
+++ javascript/examples/hello/bin/pipe.js
@@ -3,12 +3,59 @@ const readline = require('readline');
 const fs = require('fs');
 const FIFO_PATH = '/tmp/algoout';
 
-function get_response(line) {
+function start_call(line, cb) {
     try {
         request = JSON.parse(line);
-        result = call_algorithm(request);
-        content_type = 'json';
+        data = get_data(request);
 
+        Algorithm.apply(data, cb);
+    } catch (error) {
+        cb(error, null);
+    }
+}
+
+function get_data(request) {
+    if (typeof request != 'object') {
+        throw new Error('Request needs to be an object');
+    }
+
+    if (!('data' in request)) {
+        throw new Error('data was not in request');
+    }
+
+    if (!('content_type' in request)) {
+        throw new Error('content_type was not in the request');
+    }
+
+    if (request['content_type'] === 'text' || request['content_type'] === 'json') {
+        data = request['data'];
+    } else if (request['content_type'] === 'binary') {
+        data = new Buffer(request['data'], 'base64');
+    } else {
+        throw new Error('Invalid content_type: ' + request['content_type']);
+    }
+    return data;
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var alreadyWorking = false;
+function algoCallback(error, result) {
+    if (error) {
+        stacktrace = "[None]";
+        if (error instanceof Error) {
+            stacktrace = error.stack;
+        }
+
+        response = {
+            message: error.toString(),
+            stacktrace: stacktrace
+        }
+    } else {
+        content_type = 'json';
         if (Buffer.isBuffer(result)) {
             result = result.toString('base64');
             content_type = 'binary';
@@ -20,54 +67,20 @@ function get_response(line) {
                 content_type: content_type
             }
         };
-    } catch (e) {
-        response = {
-            message: e.toString(),
-            stacktrace: e.stack
-        }
     }
-    return response;
+
+    fd = fs.openSync(FIFO_PATH, 'a');
+    fs.writeSync(fd, JSON.stringify(response));
+    fs.writeSync(fd, '\n');
+    // Flushing here causes errors...
+    fs.closeSync(fd);
+    rl.write('\n');
+    alreadyWorking = false;
 }
 
-function call_algorithm(request) {
-    if (typeof request != 'object') {
-        throw new Error('Request needs to be an object');
-    }
-
-    if (!('data' in request)) {
-        throw new Error('data was not in request');
-    }
-
-    if ('content_type' in request) {
-        if (request['content_type'] === 'text' || request['content_type'] === 'json') {
-            data = request['data'];
-        } else if (request['content_type'] === 'binary') {
-            data = new Buffer(request['data'], 'base64');
-        } else {
-            throw new Error('Invalid content_type: ' + request['content_type']);
-        }
-        return Algorithm.apply(data);
-    } else {
-        throw new Error('content_type was not in the request');
-    }
-}
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-var alreadyWorking = false;
 rl.on('line', (line) => {
     if (!alreadyWorking && line.length > 0) {
         alreadyWorking = true;
-        response = get_response(line);
-        fd = fs.openSync(FIFO_PATH, 'a');
-        fs.writeSync(fd, JSON.stringify(response));
-        fs.writeSync(fd, '\n');
-        // Flushing here causes errors...
-        fs.closeSync(fd);
-        rl.write('\n');
-        alreadyWorking = false;
+        start_call(line, algoCallback);
     }
 });
--- javascript/examples/hello/src/algorithm.js
+++ javascript/examples/hello/src/algorithm.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
 
-exports.apply = function(input) {
+exports.apply = function(input, cb) {
     var sum = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
-    return "Hello user " + input + " and the sum of the first 3 integers is: " + sum;
+    cb(null, "Hello user " + input + " and the sum of the first 3 integers is: " + sum);
 };
