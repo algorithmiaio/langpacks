5f135fe856a0096b86231703fa4e46ae2daed322
James Athappilly <jamesatha@gmail.com>
2016-07-06 13:36:23 -0700

    Use base64enc for encoding and decoding. Also add support for binary output

--- r/template/bin/pipe.r
+++ r/template/bin/pipe.r
@@ -1,4 +1,6 @@
 # TODO(james): push this to setup
+install.packages("base64enc")
+library("base64enc")
 install.packages("rjson")
 library(rjson)
 
@@ -10,9 +12,18 @@ flush.console()
 
 getInputData <- function(input) {
     if (input$content_type == "binary") {
-        return base64_dec(input$data)
+        base64enc::base64decode(input$data)
+    } else {
+        input$data
     }
-    input$data
+}
+
+getResponse <- function(output) {
+    if (typeof(output) == "raw") {
+        list(result=base64enc::base64encode(output), metadata=list(content_type="binary"))
+    } else {
+        list(result=output, metadata=list(content_type="json"))
+    }
 }
 
 while (TRUE) {
@@ -31,7 +42,7 @@ while (TRUE) {
     # Flush stdout before writing back response
     flush.console()
 
-    # TODO(james): change content type, maybe? and more error checking around json serialization
-    response = list(result=output, metadata=list(content_type="json"))
+    # TODO(james): more error checking around json serialization
+    response = getResponse(output)
     writeLines(rjson::toJSON(response), con=fifo("/tmp/algoout", open="w", blocking=TRUE))
 }
