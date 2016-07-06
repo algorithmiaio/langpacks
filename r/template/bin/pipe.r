5f38b5fd5f766538f89a74352f69031caa73d861
James Athappilly <jamesatha@gmail.com>
2016-07-06 11:00:00 -0700

    Using rjson instead of jsonlite which seems to be more 'normal' when producing json objects. This also gets rid of the json templating we had before

--- r/template/bin/pipe.r
+++ r/template/bin/pipe.r
@@ -1,8 +1,8 @@
 # TODO(james): push this to setup
-install.packages("jsonlite")
-library(jsonlite)
+install.packages("rjson")
+library(rjson)
 
-conf <- fromJSON("algorithmia.conf")
+conf <- rjson::fromJSON(file="algorithmia.conf")
 source(sprintf("src/%s.r", conf$algoname))
 
 print("PIPE_INIT_COMPLETE")
@@ -16,11 +16,13 @@ while (TRUE) {
     }
 
     # TODO(james): do more error checking
-    input <- fromJSON(line)
-    response <- algorithm(input)
+    input <- rjson::fromJSON(line)
+    output <- algorithm(input)
 
     # Flush stdout before writing back response
     flush.console()
 
-    writeLines(sprintf('{ "result": %s, "metadata": { "content_type": "json" } }', toJSON(response)), con=fifo("/tmp/algoout", open="w", blocking=TRUE))
+    # TODO(james): change content type, maybe? and more error checking around json serialization
+    response = list(result=output, metadata=list(content_type="json"))
+    writeLines(rjson::toJSON(response), con=fifo("/tmp/algoout", open="w", blocking=TRUE))
 }
