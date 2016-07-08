4425fe8d70ae279c5d8103bee060d9422db9ac8a
James Athappilly <jamesatha@gmail.com>
2016-07-08 12:10:17 -0700

    Adding an option to only install the latest cran packages

--- r/template/bin/rip.py
+++ r/template/bin/rip.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python
 # rip - kinda like python's pip but for R
 
-import sys
+import argparse
 import os
 import subprocess
+import sys
 
 def main():
-    if len(sys.argv) != 2:
-        print 'ERROR: Takes 1 the file name as the only argument'
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', required=True, help='the file with the list of packages')
+    parser.add_argument('--cran-latest', action='store_true', help='only install latest CRAN packages')
+    args = parser.parse_args()
 
-    fileName = sys.argv[-1]
 
-    if not os.path.isfile(fileName):
+    if not os.path.isfile(args.file):
         print 'ERROR: The package list file does not exist'
         sys.exit(1)
 
     rscript = ['library("pacman")']
-    with open(fileName) as f:
+    with open(args.file) as f:
         for line in f.readlines():
             line = line.strip()
 
@@ -29,9 +30,11 @@ def main():
             if len(tokens) == 1: # installs the latest package from CRAN
                 rscript.append('install.packages("{}")'.format(line))
             elif tokens[0] == '-t' and len(tokens) == 2: # installs a specific archive from CRAN (most likely)
-                rscript.append('install.packages("{}", repos=NULL, type="source")'.format(tokens[1]))
+                if not args.cran_latest:
+                    rscript.append('install.packages("{}", repos=NULL, type="source")'.format(tokens[1]))
             elif tokens[0] == '-g' and len(tokens) == 2: # installs from github of the form: username/repo[/subdir][@ref|#pull]
-                rscript.append('p_install_gh(c("{}"))'.format(tokens[1]))
+                if not args.cran_latest:
+                    rscript.append('p_install_gh(c("{}"))'.format(tokens[1]))
             else:
                 print 'Unexpected line: "{}"'.format(line)
                 sys.exit(1)
