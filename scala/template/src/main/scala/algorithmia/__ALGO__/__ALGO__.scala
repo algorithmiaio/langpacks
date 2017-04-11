f6fc592c728d77752aeb0a398b945cd67ac615ff
James Athappilly <jamesatha@gmail.com>
2017-04-11 16:39:31 -0700

    Adding the scala template

---
+++ scala/template/src/main/scala/algorithmia/__ALGO__/__ALGO__.scala
@@ -0,0 +1,11 @@
+package algorithmia.__ALGO__
+
+import com.algorithmia._
+import com.algorithmia.algo._
+import com.algorithmia.data._
+
+class __ALGO__ {
+  def apply(input: String): String = {
+    "Hello " + input
+  }
+}
