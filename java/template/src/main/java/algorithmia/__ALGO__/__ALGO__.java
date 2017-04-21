3d7efa85639a6449a7eb20669f4334209e8ada08
James Athappilly <jamesatha@gmail.com>
2017-04-21 08:14:29 -0700

    Updating templates for java/scala to have generally useful stuff

--- java/template/src/main/java/algorithmia/__ALGO__/__ALGO__.java
+++ java/template/src/main/java/algorithmia/__ALGO__/__ALGO__.java
@@ -1,5 +1,11 @@
 package algorithmia.__ALGO__;
 
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
 public class __ALGO__ {
     public String apply(String s) throws Exception {
         return "Hello " + s;
--- scala/template/src/main/scala/algorithmia/__ALGO__/__ALGO__.scala
+++ scala/template/src/main/scala/algorithmia/__ALGO__/__ALGO__.scala
@@ -3,6 +3,7 @@ package algorithmia.__ALGO__
 import com.algorithmia._
 import com.algorithmia.algo._
 import com.algorithmia.data._
+import com.google.gson._
 
 class __ALGO__ {
   def apply(input: String): String = {
