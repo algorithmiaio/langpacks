4cb5ab805040555531f4c418ca3742aaa2000462
Patrick McQuighan <pmcquighan@gmail.com>
2017-09-27 16:11:11 -0700

    Use a find instead of chowning the whole directory for docker layer efficiency

--- libraries/opencv-python2/install.sh
+++ libraries/opencv-python2/install.sh
@@ -25,4 +25,4 @@ rm -rf opencv_contrib-${OPENCV_VERSION}
 ldconfig
 
 # Give algo user ability to write to updated files
-chown -R algo:algo $PYTHON_LIB_PATH/$PYTHON_VERSION/site-packages
+find $PYTHON_LIB_PATH/$PYTHON_VERSION/site-packages -user root | xargs chown algo:algo
