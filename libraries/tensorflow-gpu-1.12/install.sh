b46d089f855e53a337f9e647eff0b612452a6173
Besir Kurtulmus <besir@algorithmia.com>
2018-12-27 10:51:03 -0800

    Don't give algo user write access to packageset library

--- libraries/tensorflow-gpu-1.12/install.sh
+++ libraries/tensorflow-gpu-1.12/install.sh
@@ -4,6 +4,3 @@ set -e
 
 # Install the requirements defined in the requirements.txt file in the template
 pip install -r ../../templates/tensorflow-gpu-1.12/requirements.txt
-
-# Give algo user ability to write to updated keras/tensorflow files
-find $PYTHON_LIB_PATH/$PYTHON_VERSION/site-packages -user root | xargs chown algo:algo
