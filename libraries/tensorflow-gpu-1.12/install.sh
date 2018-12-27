db23060a4d250a3941579d566a1483a4cca71f71
Besir Kurtulmus <besir@algorithmia.com>
2018-12-27 11:00:20 -0800

    Move tensorflow-keras requirements.txt file under the context folder, and correctly refer that in the install.sh script

--- libraries/tensorflow-gpu-1.12/install.sh
+++ libraries/tensorflow-gpu-1.12/install.sh
@@ -3,4 +3,4 @@
 set -e
 
 # Install the requirements defined in the requirements.txt file in the template
-pip install -r ../../templates/tensorflow-gpu-1.12/requirements.txt
+pip install -r context/requirements.txt
