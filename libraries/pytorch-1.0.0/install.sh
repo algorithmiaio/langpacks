af1fc76fb6c0d17b149964c975ab47ec07d62a90
Besir Kurtulmus <besir@algorithmia.com>
2018-12-27 10:58:25 -0800

    Move pytorch requirements.txt file under the context folder, and correctly refer that in the install.sh script

--- libraries/pytorch-1.0.0/install.sh
+++ libraries/pytorch-1.0.0/install.sh
@@ -3,4 +3,4 @@
 set -e
 
 # Install the requirements defined in the requirements.txt file in the template
-pip install -r ../../templates/pytorch-1.0.0/requirements.txt
+pip install -r context/requirements.txt
