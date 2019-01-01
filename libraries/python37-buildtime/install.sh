95e4b8c3f9b999cce1c0a82ee45f6710242fda11
Patrick McQuighan <pmcquighan@gmail.com>
2018-12-31 16:12:18 -0800

    Python3.7 apt package doesn't exist and any other apt package would yield inconsistent builds, instead use a modified version of the official python3.7 Dockerfiles

--- libraries/python37-buildtime/install.sh
+++ libraries/python37-buildtime/install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# Install a handful of build dependencies needed for python
 set -e
 
 apt-get -y update
 
-apt-get install -u -y \
-    python3.7 \
-    python3-pip
-
-pip3 install --upgrade pip
+# Install things that are required for building python
+apt-get install --no-install-recommends -y \
+    build-essential \
+    checkinstall \
+    libbz2-dev \
+    libc6-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libncursesw5-dev \
+    libreadline-gplv2-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    tk-dev \
+    uuid-dev \
+    wget
 
 rm -rf /var/lib/apt/lists/*
