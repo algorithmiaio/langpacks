b01b3195c6d0f9a9452ce8d4057ff1d311804245
zeryx <zeryx@pop-os.localdomain>
2019-01-18 15:08:28 -0400

    got it working properly this time

--- tools/ipa_packageset_builder.py
+++ tools/ipa_packageset_builder.py
@@ -37,7 +37,7 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Creates a packageset dockerfile, by combining package templates together.')
     parser.add_argument('--base-image', dest='base_image', type=str, required=True)
-    parser.add_argument('--packages', dest='packages', type=list, required=True)
+    parser.add_argument('-p', '--package', action='append',  dest='packages', required=True)
     args = parser.parse_args()
     print(args.base_image)
     print(args.packages)
