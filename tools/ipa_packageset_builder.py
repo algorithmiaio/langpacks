9d5378709f1d57cc5cb0eb7134261b9316f44020
zeryx <zeryx@pop-os.localdomain>
2019-01-18 15:54:01 -0400

    initial mvp

--- tools/ipa_packageset_builder.py
+++ tools/ipa_packageset_builder.py
@@ -1,43 +1,74 @@
-import jinja2
+from jinja2 import Template
 import argparse
+from os import path
+from os.path import isfile
+DIR_PATH_TO_TEMPATES ="languages"
+DIR_PATH_TO_PACKAGES = "libraries"
+RUNNER_NAME = "Dockerfile.runner.j2"
+RUNNER_PATH = path.join(DIR_PATH_TO_TEMPATES, RUNNER_NAME)
+
+class Package:
+    def __init__(self, install_script=None, dockerfile_path=None, base_image=None):
+        self.install_script = install_script
+        if dockerfile_path:
+            self.dockerfile = get_dockerfile_as_string(dockerfile_path)
+        else:
+            raise Exception('dockerfile path not available.')
+        if base_image:
+            self.base_image = base_image
+        else:
+            raise Exception('base_image path not available.')
 
 
 
+def get_dockerfile_as_string(file_path):
+    with open(file_path, 'r') as fileobject:
+        stringified: str = fileobject.read()
+    return stringified
+
+def get_template(template_path):
+    with open(template_path, 'r') as fileobject:
+        template_string = fileobject.read()
+    template: Template = Template(template_string)
+    return template
+
+def save_generated_template(template, output_path):
+    with open(output_path, 'w') as fileobject:
+        fileobject.write(template)
+    return output_path
+
+def check_if_exists(filepath):
+    if isfile(filepath):
+        return filepath
+    else:
+        return None
+
+def build(base_image, package_dirs, output_file_path):
+
+    raw_template = get_template(RUNNER_PATH)
+    packages = []
+    for dir in package_dirs:
+        dockerfile_path = path.join(DIR_PATH_TO_PACKAGES, (dir, "Dockerfile"))
+        installer_path = path.join(DIR_PATH_TO_PACKAGES, (dir, "install.sh"))
+        dockerfile_path = check_if_exists(dockerfile_path)
+        installer_path = check_if_exists(installer_path)
+
+        package = Package(installer_path, dockerfile_path, base_image)
+        packages.append(package)
+    generated_template = raw_template.render(packages=packages, base_image=base_image)
+    save_generated_template(generated_template, output_file_path)
+
+    print("completed template construction, file available at {}".format(output_file_path))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Idk if you wanna use argparse but it's p easy to use.
+"""Idk if you wanna use argparse but it's p easy to use."""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Creates a packageset dockerfile, by combining package templates together.')
-    parser.add_argument('--base-image', dest='base_image', type=str, required=True)
+    parser.add_argument('-b', '--base-image', dest='base_image', type=str, required=True)
+    parser.add_argument('-o', '--output-filename', dest='output_path', required=True)
     parser.add_argument('-p', '--package', action='append',  dest='packages', required=True)
     args = parser.parse_args()
     print(args.base_image)
     print(args.packages)
+    build(args.base_image, args.packages, args.output_path)
