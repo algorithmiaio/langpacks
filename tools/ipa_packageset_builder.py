1962f7eb8adb81f799c4e861809669b039b6e89c
zeryx <zeryx@pop-os.localdomain>
2019-01-18 15:01:24 -0400

    added CLI mvp

---
+++ tools/ipa_packageset_builder.py
@@ -0,0 +1,43 @@
+import jinja2
+import argparse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Idk if you wanna use argparse but it's p easy to use.
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Creates a packageset dockerfile, by combining package templates together.')
+    parser.add_argument('--base-image', dest='base_image', type=str, required=True)
+    parser.add_argument('--packages', dest='packages', type=list, required=True)
+    args = parser.parse_args()
+    print(args.base_image)
+    print(args.packages)
