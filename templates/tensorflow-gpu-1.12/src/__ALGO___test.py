1dcb60583df6a80e1e39a8c211016e3093ec2ceb
James Sutton <james@algorithmia.com>
2019-01-24 14:27:26 -0400

    dramatically simplified template algorithm

--- templates/tensorflow-gpu-1.12/src/__ALGO___test.py
+++ templates/tensorflow-gpu-1.12/src/__ALGO___test.py
@@ -1,15 +1,7 @@
 from .__ALGO__ import apply
 
-"""
-This is the testing file, which should be bundled along with the Algorithm.py file when creating a template.
-These tests are run during the creation of a template, and if they fail - the templating process fails as well.
-"""
 
-def primariy_api_example():
-    input = {'image_url': 'https://s3.amazonaws.com/algorithmia-uploads/money_cat.jpg'}
+def test_algorithm():
+    input = "hello"
     result = apply(input)
     return result
-
-def test_api_change():
-    response = primariy_api_example()
-    assert 'prediction' in response
