846066f074c26b237cde7472ef6362642406d777
James Sutton <james@algorithmia.com>
2019-01-24 16:56:11 -0400

    made modifications to support @kenny's UI request, added lots of docs

--- templates/tensorflow-gpu-1.12/src/__ALGO__.py
+++ templates/tensorflow-gpu-1.12/src/__ALGO__.py
@@ -1,20 +1,51 @@
 
 import tensorflow.keras.backend as K
+from tensorflow import convert_to_tensor
 import numpy as np
+
+
+class InputObject:
+    def __init__(self, input_dict):
+        """
+        Creates an instance of the InputObject, which checks the format of data and throws exceptions if anything is
+        missing.
+        "matrix_a" and "matrix_b" must be the same shape.
+
+        :param A - Matrix A, converted from a json list into a keras Tensor.
+        :param B - Matrix B, converted from a json list into a keras Tensor.
+        """
+        if isinstance(input_dict, dict):
+            if {'matrix_a', 'matrix_b'} <= input_dict.keys():
+                self.A = convert(input_dict['matrix_a'])
+                self.B = convert(input_dict['matrix_b'])
+            else:
+                raise Exception("'matrix_a' and 'matrix_b' must be defined.")
+        else:
+            raise Exception('input must be a json object.')
+        if self.A.shape != self.B.shape:
+            raise Exception("the shape of matrix A must be the same as shape B.\n matrix A: {} matrix B: {}".format(
+                str(self.A.shape), str(self.B.shape)))
+
+
+def convert(list_array):
+    """
+    Converts a json list into a keras Tensor object.
+    """
+    numpy_object = np.asarray(list_array)
+    tensor_object = convert_to_tensor(numpy_object)
+    return tensor_object
 
 def apply(input):
     """
-    Multiplies two matricies together using keras, with a tensorflow-gpu backend.
-    Returns the result as the output.
+    Calculates the dot product of two matricies using keras, with a tensorflow-gpu backend.
+    Returns the product as the output.
     """
-    A = np.random.rand(10, 500)
-    B = np.random.rand(500, 6000)
+    input = InputObject(input)
 
-    x = K.variable(value=A)
-    y = K.variable(value=B)
-
-    z = K.dot(x, y)
+    z = K.dot(input.A, input.B)
     # Here you need to use K.eval() instead of z.eval() because this uses the backend session
     K.eval(z)
-    z = K.get_value(z)[0][0]
-    return "hello {}, here's your random value  {}".format(input, str(z))
+    z = K.get_value(z)
+    output = {'product': z}
+    return output
+
--- templates/tensorflow-gpu-1.12/src/__ALGO___test.py
+++ templates/tensorflow-gpu-1.12/src/__ALGO___test.py
@@ -2,6 +2,6 @@ from .__ALGO__ import apply
 
 
 def test_algorithm():
-    input = "hello"
+    input = {'matrix_a': [[0, 1], [1, 0]], 'matrix_b': [[25, 25], [11, 11]]}
     result = apply(input)
-    return result
+    assert result == {'product': [[11., 11.], [25., 25.]]}
