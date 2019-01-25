8141d69cc9ab2b958cb371f94894ae5201439a2c
James Sutton <james@algorithmia.com>
2019-01-25 12:43:59 -0400

    removed the global state debugging function call in __test.py

--- templates/tensorflow-gpu-1.12/src/__ALGO___test.py
+++ templates/tensorflow-gpu-1.12/src/__ALGO___test.py
@@ -5,6 +5,3 @@ def test_algorithm():
     input = {'matrix_a': [[0, 1], [1, 0]], 'matrix_b': [[25, 25], [11, 11]]}
     result = apply(input)
     assert result == {'product': [[11., 11.], [25., 25.]]}
-
-
-test_algorithm()
