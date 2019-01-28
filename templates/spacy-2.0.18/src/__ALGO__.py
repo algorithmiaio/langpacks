e9a3358d53bcc69ab060c02f78bd65a71df8aff1
James Sutton <james@algorithmia.com>
2019-01-28 15:01:07 -0400

    fleshed out docs a bit more

--- templates/spacy-2.0.18/src/__ALGO__.py
+++ templates/spacy-2.0.18/src/__ALGO__.py
@@ -2,11 +2,10 @@ import Algorithmia
 import spacy
 """
 This package set comes preinstalled with every available small language package provided by spacy.
-
 Pick your language from the following list: 'en', 'es', 'pt', 'fr', 'it', 'de, and 'nl'
-
 You may change the language at runtime, but bear in mind that you'll get hit with some performance loss.
 """
+LANG = "en"
 
 
 def load_spacy_lang(language):
@@ -15,6 +14,12 @@ def load_spacy_lang(language):
 
 
 def apply(input):
+    """
+    This algorithm performs "Named Entity Recognition" on the sample input document.
+    It expects the input to be an escaped string.
+    :param input: An escaped string, in the language defined by $LANG.
+    :return: a list of detected entities.
+    """
     document = nlp(input)
     named_entities = []
     for ent in document.ents:
@@ -23,4 +28,5 @@ def apply(input):
     output = {"entities found": named_entities}
     return output
 
-nlp = load_spacy_lang('en')
+
+nlp = load_spacy_lang(LANG)
