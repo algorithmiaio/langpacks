5091119a4a003e9489790e8c7f6165359c10a704
James Sutton <james@algorithmia.com>
2019-01-31 18:07:34 -0400

    initial template commit

---
+++ languages/csharp-dotnet-2.2/template/__ALGO__.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace __ALGO__
+{
+    
+    public class AlgoInput
+    {
+        public string name;        
+    }
+
+    public class AlgoOutput
+    {
+        public string output;
+    }
+    
+    class __ALGO__
+    {
+        static AlgoOutput apply(AlgoInput input)
+        {
+            string name = input.name;
+            AlgoOutput output = new AlgoOutput {output = "Hello " + name}; 
+            return output;
+        }
+        static void Main()
+        {
+            AlgoInput test = new AlgoInput {name = "James"};
+            AlgoOutput output = apply(test);
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(output));
+        }
+    }
+    
+}
