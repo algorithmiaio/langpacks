8060e0dca61fbf0d75ea254329cf19f0ca0d49a2
James Sutton <james@algorithmia.com>
2019-02-26 13:57:27 -0400

    removed `Pipe` project from the runtime image, as we've now bundled the Pipe into a nuget package.

---
+++ languages/csharp-dotnet-2.2/template/src/pipe.cs
@@ -0,0 +1,17 @@
+using __ALGO__;
+using Algorithmia;
+
+// This file takes your __ALGO__.cs file, loads the apply function and creates a Pipe wrapper around it.
+// WARNING: Do not remove this file from your project, it will make your algorithm unusable.
+
+namespace __ALGO__
+
+{
+    public class pipe
+    {
+        static int Main()
+        {
+            return AlgorithmiaPipe.Pipe.Enter(typeof(__ALGO__));
+        }
+    }
+}
