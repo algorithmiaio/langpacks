57b596f0eaf59d8d1c070864f285d2c5283884a7
James Sutton <james@algorithmia.com>
2019-02-27 13:14:36 -0400

    removed unnecessary xml tags

--- languages/csharp-dotnet-2.2/template/src/__ALGO__.cs
+++ languages/csharp-dotnet-2.2/template/src/__ALGO__.cs
@@ -10,28 +10,22 @@ using Algorithmia;
 namespace __ALGO__
 {
     public class AlgoInput
-    {/// <summary>
+    {
+        
      /// The input object type for your algorithm, this must contain json serializable types.
-     /// </summary>
         public string name;
     }
 
     public class AlgoOutput
     {
-        /// <summary>
         /// This is the output object type for your algorithm, it can contain optional types, but they all must be json serializable.
-        /// </summary>
         public string output;
     }
     
     class __ALGO__
     {
-        /// <summary>
         /// API calls will begin at the apply() method, with the request body passed as 'input'
         /// For more details, see algorithmia.com/developers/algorithm-development/languages
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
         static public AlgoOutput apply(AlgoInput input)
         {
             string name = input.name;
