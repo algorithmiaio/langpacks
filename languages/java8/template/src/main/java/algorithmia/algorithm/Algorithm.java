c6539287f391a99740c2f068c619666fce7d5dc6
James Sutton <james@algorithmia.com>
2019-05-01 15:33:58 -0300

    addressed comments from @anowell and @pcmq

--- languages/java8/template/src/main/java/algorithmia/algorithm/Algorithm.java
+++ languages/java8/template/src/main/java/algorithmia/algorithm/Algorithm.java
@@ -1,19 +1,27 @@
-package algorithmia;
+package algorithmia.algorithm;
 import com.algorithmia.algorithmHandler.*;
 import com.algorithmia.*;
 import java.util.HashMap;
 
+
+class AdvancedInput{
+    String name;
+    Integer age;
+}
+
+
 public class Algorithm {
-    String Apply(String input, HashMap<String, String> context) throws Exception{
+    String Apply(AdvancedInput input, HashMap<String, String> context) throws Exception{
             if(context.containsKey("local_file")){
-                return "hello ".concat(input) + " your model file is downloaded here ".concat(context.get("local_file"));
+                return "Hello " + input.name + " you are " + input.age +
+                        " years old, and your model file is downloaded here " + context.get("local_file");
             }
-        return "hello ".concat(input);
+        return "hello " + input.name+ " you are " + input.age + " years old";
     }
     HashMap<String, String> DownloadModel() throws Exception{
             HashMap<String, String> context = new HashMap<>();
-            AlgorithmiaClient client = Algorithmia.client(System.getenv("ALGORITHMIA_API_KEY"));
-            String localFile = client.file("data://.my/collection/testfile.json").getFile().getName();
+            AlgorithmiaClient client = Algorithmia.client();
+            String localFile = client.file("data://demo/collection/testfile.json").getFile().getName();
             context.put("local_file", localFile);
             return context;
     }
