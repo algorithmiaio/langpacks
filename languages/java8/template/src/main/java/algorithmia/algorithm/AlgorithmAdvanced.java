b771cb73b5527c28b9c8945cddd8ede9dadd8ccb
James Sutton <james@algorithmia.com>
2019-05-08 17:34:02 -0300

    fixed formatting

---
+++ languages/java8/template/src/main/java/algorithmia/algorithm/AlgorithmAdvanced.java
@@ -0,0 +1,37 @@
+package algorithmia.algorithm;
+
+import com.algorithmia.algorithmHandler.*;
+import com.algorithmia.*;
+
+import java.util.HashMap;
+
+
+public class AlgorithmAdvanced {
+
+    class AdvancedInput {
+        String name;
+        Integer age;
+    }
+
+    String Apply(AdvancedInput input, HashMap<String, String> context) throws Exception {
+        if (context.containsKey("local_file")) {
+            return "Hello " + input.name + " you are " + input.age +
+                    " years old, and your model file is downloaded here " + context.get("local_file");
+        }
+        return "hello " + input.name + " you are " + input.age + " years old";
+    }
+
+    HashMap<String, String> DownloadModel() throws Exception {
+        HashMap<String, String> context = new HashMap<>();
+        AlgorithmiaClient client = Algorithmia.client();
+        String localFile = client.file("data://demo/collection/testfile.json").getFile().getName();
+        context.put("local_file", localFile);
+        return context;
+    }
+
+    public static void main(String[] args) throws Exception {
+        AlgorithmAdvanced defs = new AlgorithmAdvanced();
+        AlgorithmHandler algo = new AlgorithmHandler<>(defs, defs::Apply, defs::DownloadModel);
+        algo.run();
+    }
+}
--- languages/java8/template/src/main/java/algorithmia/algorithm/AlgorithmBasic.java
+++ languages/java8/template/src/main/java/algorithmia/algorithm/AlgorithmBasic.java
@@ -1,14 +1,14 @@
 package algorithmia.algorithm;
+
 import com.algorithmia.algorithmHandler.*;
 import com.algorithmia.*;
 
-class AlgorithmBasic
-{
-    String Apply(String input) throws Exception{
+class AlgorithmBasic {
+    String Apply(String input) throws Exception {
         return "hello ".concat(input);
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         AlgorithmBasic defs = new AlgorithmBasic();
         AlgorithmHandler algo = new AlgorithmHandler<>(defs, defs::Apply);
         algo.run();
