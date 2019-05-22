3107f44de10d85b594b619651de06ea8fe83c81a
James Sutton <james@algorithmia.com>
2019-05-22 18:49:56 -0300

    removed actual work in load, adjusted documentation

--- languages/java11/template/src/main/java/com/algorithmia/algorithm/Algorithm.java
+++ languages/java11/template/src/main/java/com/algorithmia/algorithm/Algorithm.java
@@ -3,18 +3,23 @@ package com.algorithmia.algorithm;
 import com.algorithmia.development.*;
 import com.algorithmia.*;
 
+import java.util.HashMap;
+
 
 // This class defines your algorithm.
 class Algorithm extends AbstractAlgorithm<String, String>{
 
+//     HashMap<String, String> context = new HashMap<>();
+    
 
     public String apply(String input){
         return "hello ".concat(input);
     }
 
     // For more information around how loading works, please check out the algorithm docs in algorithmia.com/developers/
+    //<--- Implement this for loading functionality -->
     public void load(){
-        System.out.println("Implement this for loading functionality");
+        // context.put("loaded_model_file") = ...
     }
 
     public static void main(String[] args) {
