0d9131792ce04a6756b8efea9fb6f5e6d4039b0c
Field Cady <fieldc@allenai.org>
2019-06-18 12:51:42 -0700

    dockerfile working for phantomjs & chrome (#91)
    * dockerfile working for phantomjs
    
    * Chrome support
    
    * oops, fixing a bug
    
    * small edit to coment
    
    * removing Docker lines that will get added in on the backend
    
    * example code for selenium
    
    * patricks comments - follow up
    
    * patricks comments - follow up
    
    * adding version of selenium
    
    * patricks comments - follow up
    
    * cleaning up pip installs tuff

---
+++ templates/selenium-python37/src/__ALGO__.py
@@ -0,0 +1,31 @@
+import Algorithmia
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def get_chrome():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    return driver
+
+def get_phantomjs():
+    driver = webdriver.PhantomJS()
+    return driver
+
+# API calls will begin at the apply() method, with the request body passed as 'input'
+# For more details, see algorithmia.com/developers/algorithm-development/languages
+def apply(input):
+    driver = get_chrome()
+    #driver = get_phantomjs()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    driver.close()
+    return {"status": "success"}
---
+++ templates/selenium-python37/src/__ALGO___test.py
@@ -0,0 +1,8 @@
+from __ALGO__ import apply
+
+def test_algorithm():
+    input = {}
+    result = apply(input)
+    assert result == {"status": "success"}
+
+test_algorithm()
