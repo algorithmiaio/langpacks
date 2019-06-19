a177089857794569d371edf9906aba0bdc2d1023
field-cady <fieldcady@Fields-MacBook-Pro.local>
2019-06-19 11:23:08 -0700

    improving template to incoporate feedback

--- templates/selenium-python37/src/__ALGO__.py
+++ templates/selenium-python37/src/__ALGO__.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def get_chrome():
+    # Chromium must be launched in headless mode
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1420,1080')
@@ -12,20 +13,26 @@ def get_chrome():
     return driver
 
 def get_phantomjs():
+    # PhantomJS is already headless
     driver = webdriver.PhantomJS()
     return driver
 
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
+     # note: you MUST have internet access enabled for this test work
     driver = get_chrome()
     #driver = get_phantomjs()
     driver.get("http://www.python.org")
-    assert "Python" in driver.title
+    if 'Python' not in driver.title:
+        return {"status": "failure",
+            "message": "failed to load page"}
     elem = driver.find_element_by_name("q")
     elem.clear()
     elem.send_keys("pycon")
     elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
+    if 'No results found' not in driver.page_source:
+        return {"status": "failure",
+            "message": "Failed to find required text in page body"}
     driver.close()
     return {"status": "success"}
