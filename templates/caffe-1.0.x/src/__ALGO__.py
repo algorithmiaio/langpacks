3b7a8602ca736a813d86221685dc27ec4f55fb44
field-cady <fieldcady@Fields-MacBook-Pro.local>
2019-06-20 15:08:17 -0700

    simple example code

---
+++ templates/caffe-1.0.x/src/__ALGO__.py
@@ -0,0 +1,7 @@
+import Algorithmia
+import caffe
+
+# API calls will begin at the apply() method, with the request body passed as 'input'
+# For more details, see algorithmia.com/developers/algorithm-development/languages
+def apply(input):
+    return "hello {}".format(input)
