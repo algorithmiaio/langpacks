5cb3a3e17e4d86f15a3f3809eed4331255919726
James Sutton <james@algorithmia.com>
2019-10-18 14:29:31 -0300

    renamed anaconda to proper version, upgraded anacodna version

--- languages/anaconda3/template/src/Algorithm.py
+++ languages/anaconda3/template/src/Algorithm.py
@@ -1,7 +1,7 @@
 import Algorithmia
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
-
+# Test
 def apply(input):
     return "hello {}".format(str(input))
 
