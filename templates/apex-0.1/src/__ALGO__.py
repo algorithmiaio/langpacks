52bc8cce961c1f506a70aa8cef823a8a529b0f35
Besir Kurtulmus <besirkurtulmus@gmail.com>
2020-03-09 15:21:12 -0700

    Fix the apex installation (#134)
    * Fix the apex installation
    
    * Fix requirements.txt for apex
    
    * Add initial template for apex-0.1
    
    * Remove apt-get purge for cuda stuff in apex-0.1
    
    * Add initial tempalte for Nvidia apex
    
    * Remove unused variables

--- templates/apex-0.1/src/__ALGO__.py
+++ templates/apex-0.1/src/__ALGO__.py
@@ -1,9 +1,34 @@
 import Algorithmia
-import apex
+import torch as th
+from apex import amp
 
 """
-This package set comes preinstalled with Nvidia Apex 0.1.
+This package set comes preinstalled with Nvidia Apex 0.1
+
+Example Input:
+{
+    "vector": [
+    	[0, 1, 2, 3],
+    	[4, 5, 6, 7],
+    	[8, 9, 10, 11],
+    	[12, 13, 14, 15]
+    ]
+}
 """
+D_in, D_out = 4, 4
+
+model = th.nn.Linear(D_in, D_out).cuda()
+optimizer = th.optim.SGD(model.parameters(), lr=1e-3)
+
+model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
+
+def convert(vector):
+	th_tensor = th.tensor(vector).float()
+	gpu_tensor = th_tensor.cuda()
+	return gpu_tensor
 
 def apply(input):
-    return "Hello {}!".format(input)
+	x = convert(input["vector"])
+	res = model(x)
+	serialized_res = x.cpu().numpy().tolist()
+	return {"result": serialized_res}
