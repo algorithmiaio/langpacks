f66d99287b8826c1fa04b83e822f733fb56d9681
Daniel Rodriguez <daniel@danielfrg.com>
2020-11-17 10:51:38 -0500

    Add miniforge3 language

---
+++ languages/miniforge3/template/src/__ALGO__.py
@@ -0,0 +1,6 @@
+import Algorithmia
+
+# API calls will begin at the apply() method, with the request body passed as 'input'
+# For more details, see algorithmia.com/developers/algorithm-development/languages
+def apply(input):
+    return "hello {}".format(input)
--- tools/template_manager.py
+++ tools/template_manager.py
@@ -8,36 +8,40 @@ RUNNER_NAME = "Dockerfile.runner.j2"
 BUILDER_NAME = "Dockerfile.builder.j2"
 COMPILE_NAME = "Dockerfile.compile.j2"
 LANGSERVER_VERSION = "b35efaa7d69d24efcd83e866b7445446f92eb0d6"  # Update this when we change how langserver works.
-LANGSERVER_IMAGE ="algorithmiahq/langserver:{}".format(LANGSERVER_VERSION)
+LANGSERVER_IMAGE = "algorithmiahq/langserver:{}".format(LANGSERVER_VERSION)
 RUNNER_PATH = path.join(DIR_PATH_TO_TEMPATES, RUNNER_NAME)
 BUILDER_PATH = path.join(DIR_PATH_TO_TEMPATES, BUILDER_NAME)
 COMPILE_PATH = path.join(DIR_PATH_TO_TEMPATES, COMPILE_NAME)
 
+
 class Package:
     def __init__(self, package_name, install_script, dockerfile_path):
+        self.package_name = package_name
         self.script = install_script
         if dockerfile_path:
             self.dockerfile = get_dockerfile_as_string(dockerfile_path)
         else:
-            raise Exception('dockerfile path not available for package {}'.format(package_name))
+            raise Exception(
+                "dockerfile path not available for package {}".format(package_name)
+            )
 
 
 def get_dockerfile_as_string(file_path):
-    with open(file_path, 'r') as fileobject:
+    with open(file_path, "r") as fileobject:
         stringified = fileobject.read()
-    output = stringified.split('\n')
+    output = stringified.split("\n")
     return output
 
 
 def get_template(template_path):
-    with open(template_path, 'r') as fileobject:
+    with open(template_path, "r") as fileobject:
         template_string = fileobject.read()
     template = Template(template_string)
     return template
 
 
 def save_generated_template(template, output_path):
-    with open(output_path, 'w') as fileobject:
+    with open(output_path, "w") as fileobject:
         fileobject.write(template)
     return output_path
 
@@ -49,7 +53,9 @@ def check_if_exists(filepath):
         return None
 
 
-def generate_compile_image(builder_image_name, runner_image_name, config_data, output_file_path):
+def generate_compile_image(
+    builder_image_name, runner_image_name, config_data, output_file_path
+):
     raw_template = get_template(COMPILE_PATH)
     generated_template = raw_template.render(
         builder_image=builder_image_name,
@@ -58,6 +64,9 @@ def generate_compile_image(builder_image_name, runner_image_name, config_data, o
         local=True,
     )
     save_generated_template(generated_template, output_file_path)
+    print(
+        "completed template construction, file available at {}".format(output_file_path)
+    )
     return output_file_path
 
 
@@ -67,7 +76,9 @@ def generate_intermediate_image(base_image, package_dirs, output_file_path, mode
     elif mode == "buildtime":
         raw_template = get_template(BUILDER_PATH)
     else:
-        raise Exception("we did not recieve a valid 'mode', it must be either 'runtime' or 'buildtime'.")
+        raise Exception(
+            "we did not recieve a valid 'mode', it must be either 'runtime' or 'buildtime'."
+        )
     packages = []
     for dir in package_dirs:
         dockerfile_path = path.join(DIR_PATH_TO_PACKAGES, dir, "Dockerfile")
@@ -80,8 +91,11 @@ def generate_intermediate_image(base_image, package_dirs, output_file_path, mode
     generated_template = raw_template.render(
         packages=packages,
         base_image=base_image,
-        langpacks_version='',
-        langserver_image=LANGSERVER_IMAGE)
+        langpacks_version="",
+        langserver_image=LANGSERVER_IMAGE,
+    )
     save_generated_template(generated_template, output_file_path)
-    print("completed template construction, file available at {}".format(output_file_path))
+    print(
+        "completed template construction, file available at {}".format(output_file_path)
+    )
     return output_file_path
