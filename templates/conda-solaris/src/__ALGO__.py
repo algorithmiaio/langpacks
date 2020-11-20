5d99cb109c5e4aebd35d1a3d4cdf879f8ebea40b
Daniel Rodriguez <daniel@danielfrg.com>
2020-11-20 14:50:55 -0500

    Remove conda from this PR

--- templates/conda-solaris/src/__ALGO__.py
+++
@@ -1,7 +0,0 @@
-import Algorithmia
-import solaris
-
-# API calls will begin at the apply() method, with the request body passed as 'input'
-# For more details, see algorithmia.com/developers/algorithm-development/languages
-def apply(input):
-    return "hello {}".format(input)
