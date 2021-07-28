d6bf2e78a4ee221c1e74a4d726b3e46c822382e5
James Sutton <1892175+zeryx@users.noreply.github.com>
2021-07-28 11:05:47 -0700

    ALGO-1054 added more doc links in the template (#208)
    * added more doc links in the template
    
    * Update __ALGO__.py
    
    - Following PEP 8 conventions w/ module docstring at top
    - I think we either need to show the code example or not mention load() at all. It's completely confusing without context.
    - I don't think we should use method/function docstrings to put comments about how the ADK works. The body of the actual algorithm code should be kept clean for the developer.
    
    * Update __ALGO__.py
    
    indent docstring code sample
    
    Co-authored-by: lemonez <36384768+lemonez@users.noreply.github.com>

--- languages/python3/template/src/__ALGO__.py
+++ languages/python3/template/src/__ALGO__.py
@@ -1,18 +1,36 @@
+"""Algorithm Development Kit (ADK) algorithm-creation template.
+
+* This template uses Algorithmia's ADK module to provide structure for algorithm development. For details see:
+      https://algorithmia.com/developers/algorithm-development/languages/python#what-is-an-algorithm-development-kit-adk
+      https://github.com/algorithmiaio/algorithmia-adk-python
+
+* API calls begin at the `apply` function, with the JSON request body deserialized and passed as `input`.
+
+* The instantiation of an `ADK` object is what turns your library code into an algorithm that can run on Algorithmia.
+
+* The `ADK.init` method is what actually starts the algorithm. To explore further, see the source code linked above.
+
+* If the `apply` function uses state that's loaded into memory via a `load` function, you can pass that
+  loaded state to your `apply` function by defining an optional `globals` parameter, i.e.:
+
+      ...
+      def apply(input, globals):
+          return "hello {} {}".format(str(input), str(globals["payload"]))
+
+      def load():
+          globals = {}
+          globals["payload"] = "Loading has been completed."
+          return globals
+
+      algorithm = ADK(apply, load)
+      ...
+"""
 from Algorithmia import ADK
 
 
-# API calls will begin at the apply() method, with the request body passed as 'input'
-# For more details, see algorithmia.com/developers/algorithm-development/languages
-
 def apply(input):
-    # If your apply function uses state that's loaded into memory via load, you can pass that loaded state to your apply
-    # function by defining an additional "globals" parameter in your apply function; but it's optional!
     return "hello {}".format(str(input))
 
 
-# This turns your library code into an algorithm that can run on the platform.
-# If you intend to use loading operations, remember to pass a `load` function as a second variable.
 algorithm = ADK(apply)
-# The 'serve()' function actually starts the algorithm, you can follow along in the source code
-# to see how everything works.
 algorithm.init("Algorithmia")
