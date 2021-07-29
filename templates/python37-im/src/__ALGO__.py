be52a05c68e08760bb6cf5f0f2933f5d75190c48
zeryx <1892175+zeryx@users.noreply.github.com>
2021-07-29 12:37:56 -0300

    updated deps to use custom workflow

--- templates/python37-im/src/__ALGO__.py
+++ templates/python37-im/src/__ALGO__.py
@@ -1,111 +1,58 @@
 import Algorithmia
-from PIL import Image
+# from PIL import Image
 import shutil
 from wand.image import Image as Magic
-import urllib
-from six import string_types
+from uuid import uuid4
+import os
 
-"""
-Example payload:
-{"input":"http://www.africau.edu/images/default/sample.pdf", "output":"sample.png", "collection":"data://.algo/temp"}
-"""
-
-class AlgorithmError(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
+ALGO_IMAGE_DIRECTORY = 'data://pl_data_science_build/convert_image_file_type_store/'
 
 client = Algorithmia.client()
 
 
-# signature from http://stackoverflow.com/questions/23706661/imagemagick-wand-save-pdf-pages-as-images
-def pdfToImage(data, out_path):
-    extension = getExtension(out_path).split(".")[1]
-    with open(data, 'rb') as f:
-        with Magic(blob=f) as pdf:
-            pages = len(pdf.sequence)
-            print("pdf pages: {}\n pdf width: {}\n pdf height: {}".format(pages, pdf.width, pdf.height))
-            image = Magic(width=pdf.width + int(pdf.width / 2), height=pdf.height * pages)
-            for i in range(pages):
-                image.composite(
-                    pdf.sequence[i],
-                    top=pdf.height * i,
-                    left=0)
-            image.make_blob(extension)
-            image.save(filename=out_path)
-    return out_path
-
-
-def downloadFile(urlData):
-    if isinstance(urlData, string_types):
-        # if not os.path.isdir("/tmp/files"):
-        dir = "/tmp/"
-        if urlData.startswith("http://") or urlData.startswith("https://"):
-            (response, _) = urllib.request.urlretrieve(urlData, dir + getFileName(urlData))
-            noSpaces = response.replace(' ', '-')
-            os.rename(response, noSpaces)
-            return noSpaces
-        if re.search("[\\w\\+]+://.+", urlData):
-            dataf = client.file(urlData)
-            noSpaces = dir + dataf.getName().replace(' ', '-')
-            response = dataf.getFile().name
-            os.rename(response, noSpaces)
-            return noSpaces
-        else:
-            raise AlgorithmError("Please provide a valid data://, http:// or https:// URL.")
-    else:
-        raise AlgorithmError("input must be a URL or URI")
-
-
-def saveFileToData(local, outName, collection):
-    out = collection + '/' + outName
-    client.file(out).putFile(local)
-    return out
-
-
-def cleanUp(output_path):
-    shutil.rmtree('/tmp/files')
-    os.remove(output_path)
-
-
-def getEncoding(file):
-    proc = subprocess.Popen(['file', '-bi', file], stdout=subprocess.PIPE)
-    out, _ = proc.communicate()
-    breaking = out.split("; ")[1]
-    body = breaking.split("=")[1]
-    print(body)
-    return body
-
-
-def getFileName(path):
-    if path.startswith("http://") or path.startswith("https://"):
-        path = urllib.parse.urlsplit(path).path
-        filename = posixpath.basename(path)
-        return filename
-    else:
-        filename = os.path.basename(path)
-        return filename
-
-
-def getExtension(fileName):
-    return os.path.splitext(fileName)[1]
+def convert_image(local_image_file, output_extension):
+    unique_prefix = str(uuid4())  # unique name to assign to temporary local files
+    local_images = []  # list to hold the resulting names of images
+    with open(local_image_file, 'rb') as f:
+        binary_data = f.read()
+        with Magic(blob=binary_data, resolution=200) as image_stream:
+            page_count = len(image_stream.sequence)
+            for page in range(page_count):
+                with Magic(image_stream.sequence[page]) as image:
+                    image.make_blob(output_extension)
+                    image_name = unique_prefix + '_' + str(page + 1) + '.' + output_extension
+                    image.save(filename='/tmp/' + image_name)
+                    local_images.append(image_name)
+    return local_images
 
 
 def apply(json):
-    if isinstance(json, dict):
-        if "collection" in json and "input" in json and "output" in json:
-            collection = json["collection"]
-            input = json["input"]
-            o_filename = json["output"]
-            file = downloadFile(input)
-            local_output_path = "/tmp/" + o_filename
-            pdf = imageToPDF(file, local_output_path)
-            result = saveFileToData(pdf, o_filename, collection)
-            return result
-        else:
-            raise AlgorithmError("algorithm input must contain: \"input\", \"collection\", and \"output\"")
-    else:
-        raise AlgorithmError("input must be json.")
+    remote_file = json["inputFile"]  # file stored on Algorithmia's datastore (not local to docker)
+    output_extension = json['outputExtension']
+
+    local_input_file = client.file(remote_file).getFile().name  # this is actually something like "/tmp/tmpk89y2ip6"
+
+    converted_images = convert_image(local_input_file,
+                                     output_extension)  # result will be a list of local image names (could be a list of one)
+    results = []
+    for i in converted_images:
+        full_remote_path = ALGO_IMAGE_DIRECTORY + i
+        client.file(full_remote_path).putFile('/tmp/' + i)
+        results.append(full_remote_path)
+        os.remove(local_input_file)
+        os.remove('/tmp/' + i)
+    delete_files = []
+
+    # Remove imagemagick files on /tmp
+    for f in os.listdir('/tmp'):
+        if f.lower().startswith('magick'):
+            delete_files.append(os.path.join('/tmp', f))
+            os.remove(os.path.join('/tmp', f))
+
+    result = {'original_name': i,
+              'delete_files': delete_files,
+              'temp_name': local_input_file,
+              'space': shutil.disk_usage('/tmp/'),
+              'tmp_contents': os.listdir('/tmp/')
+              }
+    return result
