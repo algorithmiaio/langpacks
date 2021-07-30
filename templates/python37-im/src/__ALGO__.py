e95ec94ee7b55039b612ead188cd74c26dd6107b
zeryx <1892175+zeryx@users.noreply.github.com>
2021-07-30 17:23:23 -0300

    existing python update doesn't seem to solve issue, fixing

--- templates/python37-im/src/__ALGO__.py
+++ templates/python37-im/src/__ALGO__.py
@@ -13,16 +13,15 @@ client = Algorithmia.client()
 def convert_image(remote_file, client, output_extension):
     unique_prefix = str(uuid4())  # unique name to assign to temporary local files
     local_images = []  # list to hold the resulting names of images
-    with client.file(remote_file) as f:
-        binary_data = f.read()
-    with Magic(blob=binary_data, resolution=200) as image_stream:
-        page_count = len(image_stream.sequence)
-        for page in range(page_count):
-            with Magic(image_stream.sequence[page]) as image:
-                image.make_blob(output_extension)
-                image_name = unique_prefix + '_' + str(page + 1) + '.' + output_extension
-                image.save(filename='/tmp/' + image_name)
-                local_images.append(image_name)
+    with client.file(remote_file).getFile() as f:
+        with Magic(blob=f.read(), resolution=200) as image_stream:
+            page_count = len(image_stream.sequence)
+            for page in range(page_count):
+                with Magic(image_stream.sequence[page]) as image:
+                    image.make_blob(output_extension)
+                    image_name = unique_prefix + '_' + str(page + 1) + '.' + output_extension
+                    image.save(filename='/tmp/' + image_name)
+                    local_images.append(image_name)
     return local_images
 
 
