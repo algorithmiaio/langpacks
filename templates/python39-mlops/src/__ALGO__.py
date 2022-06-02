339cce7e2d14b5ebc5df645c36d1a6bc37259548
zeryx <1892175+zeryx@users.noreply.github.com>
2022-06-01 21:02:56 -0300

    initial mlops commit

--- templates/python39-mlops/src/__ALGO__.py
+++ templates/python39-mlops/src/__ALGO__.py
@@ -26,10 +26,25 @@
       ...
 """
 from Algorithmia import ADK
+from datarobot.mlops.mlops import MLOps
+import os
+import pandas as pd
+
+os.environ["MLOPS_DEPLOYMENT_ID"] = "6126e05f97b19e6599ac1f39"
+os.environ["MLOPS_MODEL_ID"] = "6126df6a78aa5cb610acea36"
+os.environ["MLOPS_SPOOLER_TYPE"] = "FILESYSTEM"
+os.environ["MLOPS_FILESYSTEM_DIRECTORY"] = "/tmp/ta"
+mlops = MLOps().init()
 
 
 def apply(input):
-    return "hello {}".format(str(input))
+    df = pd.DataFrame(columns=['id', 'values'])
+    df.loc[0] = ["abcd", 0.25]
+    association_ids = df.iloc[:, 0].tolist()
+    reporting_predictions = [0.25]
+    mlops.report_deployment_stats(100, 15)
+    mlops.report_predictions_data(frame_df=df, predictions=reporting_predictions, association_ids=association_ids)
+    return reporting_predictions
 
 
 algorithm = ADK(apply)
