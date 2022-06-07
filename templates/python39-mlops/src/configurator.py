0126292f56c6c197c5f850084b205450ce85970a
zeryx <1892175+zeryx@users.noreply.github.com>
2022-06-07 16:54:41 -0300

    reopening existing file

--- templates/python39-mlops/src/configurator.py
+++ templates/python39-mlops/src/configurator.py
@@ -7,10 +7,11 @@ agents_dir = "/opt/mlops-agent/datarobot_mlops_package-8.1.2"
 DATAROBOT_API_TOKEN = os.getenv('DATAROBOT_API_TOKEN', None)
 DATAROBOT_ENDPOINT = "https://app.datarobot.com"
 
-with open(f'{agents_dir}/conf/mlops.agent.conf.yaml', 'w') as f:
+with open(f'{agents_dir}/conf/mlops.agent.conf.yaml') as f:
     documents = yaml.load(f, Loader=yaml.FullLoader)
     documents['mlopsUrl'] = DATAROBOT_ENDPOINT
     documents['apiToken'] = DATAROBOT_API_TOKEN
+with open(f'{agents_dir}/conf/mlops.agent.conf.yaml', 'w') as f:
     yaml.dump(documents)
 
 subprocess.call(f'{agents_dir}/bin/start-agent.sh')
