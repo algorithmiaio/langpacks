fe3a4101db99608496aaa519ce2c9b3150c37057
zeryx <1892175+zeryx@users.noreply.github.com>
2022-06-07 11:56:18 -0300

    added dockerfile, updated template

--- templates/python39-mlops/src/__ALGO__.py
+++ templates/python39-mlops/src/__ALGO__.py
@@ -1,30 +1,3 @@
-"""Algorithm Development Kit (ADK) algorithm-creation template.
-
-* This template uses Algorithmia's ADK module to provide structure for algorithm development. For details see:
-      https://algorithmia.com/developers/algorithm-development/languages/python#what-is-an-algorithm-development-kit-adk
-      https://github.com/algorithmiaio/algorithmia-adk-python
-
-* API calls begin at the `apply` function, with the JSON request body deserialized and passed as `input`.
-
-* The instantiation of an `ADK` object is what turns your library code into an algorithm that can run on Algorithmia.
-
-* The `ADK.init` method is what actually starts the algorithm. To explore further, see the source code linked above.
-
-* If your algorithm relies on in-memory data (e.g., large model files) that are the same every time the algorithm
-  is executed, you can load those data into a `globals` object in a `load` function and pass that to `apply`, i.e.:
-
-      ...
-      def apply(input, globals):
-          return "hello {} {}".format(str(input), str(globals["payload"]))
-
-      def load():
-          globals = {}
-          globals["payload"] = "Loading has been completed."
-          return globals
-
-      algorithm = ADK(apply, load)
-      ...
-"""
 from Algorithmia import ADK
 from datarobot.mlops.mlops import MLOps
 import os
@@ -38,13 +11,13 @@ os.environ["MLOPS_FILESYSTEM_DIRECTORY"] = "/tmp/ta"
 mlops = MLOps().init()
 
 
-def apply(input):
+def apply():
     df = pd.DataFrame(columns=['id', 'values'])
     df.loc[0] = ["abcd", 0.25]
     association_ids = df.iloc[:, 0].tolist()
     reporting_predictions = [0.25]
     mlops.report_deployment_stats(100, 15)
-    mlops.report_predictions_data(frame_df=df, predictions=reporting_predictions, association_ids=association_ids)
+    mlops.report_predictions_data(features_df=df, predictions=reporting_predictions, association_ids=association_ids)
     return reporting_predictions
 
 
