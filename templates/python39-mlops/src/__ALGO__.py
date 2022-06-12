fb50733c0564a669a8bd70eb73571dc903406de5
zeryx <1892175+zeryx@users.noreply.github.com>
2022-06-11 21:46:34 -0300

    manifest update for testing

--- templates/python39-mlops/src/__ALGO__.py
+++ templates/python39-mlops/src/__ALGO__.py
@@ -4,15 +4,19 @@ import os
 import pandas as pd
 
 
-def apply(input):
+def load(state):
+    state['mlops'] = MLOps().init()
+    return state
+
+def apply(input, state):
     df = pd.DataFrame(columns=['id', 'values'])
     df.loc[0] = ["abcd", 0.25]
     association_ids = df.iloc[:, 0].tolist()
     reporting_predictions = [0.25]
-    mlops.report_deployment_stats(100, 15)
-    mlops.report_predictions_data(features_df=df, predictions=reporting_predictions, association_ids=association_ids)
+    state['mlops'].report_deployment_stats(100, 15)
+    state['mlops'].report_predictions_data(features_df=df, predictions=reporting_predictions, association_ids=association_ids)
     return reporting_predictions
 
 
-algorithm = ADK(apply, mlops=True)
+algorithm = ADK(apply, load, mlops=True)
 algorithm.init("Algorithmia")
