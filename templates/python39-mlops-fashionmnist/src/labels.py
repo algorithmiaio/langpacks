a05460a5a0d61d922fb6b59fadfd8ddfd990bdf9
James Sutton <1892175+zeryx@users.noreply.github.com>
2022-07-26 07:49:59 -0700

    forgot the labels.py file (#234)
    🤦‍♂️

---
+++ templates/python39-mlops-fashionmnist/src/labels.py
@@ -0,0 +1,12 @@
+labels_map = [
+        "T-Shirt",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle Boot"
+    ]
